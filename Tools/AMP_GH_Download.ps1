1bb2e58404b1643f7da26dd2c9ad9ef4350ce07c
James Manker <jamesmanker@pm.me>
2022-05-22 15:59:48 -0500

    AMP Templates Download Tool
    This is a draft to attempt running the AMP GH Download Script automatically. I fully understand this might be closed. :)

---
+++ Tools/AMP_GH_Download.ps1
@@ -0,0 +1,27 @@
+﻿[string]$ADS = ampinstmgr -i ADS | Select-String -Pattern "Data Path"
+$ADSPath = $ADS.Substring(21)
+$TemplatesPath = $ADSPath + "\Plugins\ADSModule\GenericTemplates"
+$ZipPath = $TemplatesPath + "\AMPTemplates-main"
+
+Write-Output "ADS Path $ADS"
+Write-Output "Change directory to $TemplatesPath"
+cd "$TemplatesPath"
+
+Write-Output "Downloading AMPTemplates GH"
+Invoke-WebRequest 'https://github.com/CubeCoders/AMPTemplates/archive/refs/heads/main.zip' -OutFile .\amptemplates.zip
+
+Write-Output "Extracting downloaded zip file"
+Expand-Archive .\amptemplates.zip .\ -Force
+
+Write-Output "Deleting downloaded zip file"
+Remove-Item .\amptemplates.zip
+
+Write-Output "Moving files into main template directory"
+Get-ChildItem -Path $ZipPath -Recurse -File | Move-Item -Destination $TemplatesPath -Force
+
+Write-Output "Removing unnecessary items"
+Remove-Item $TemplatesPath\*.md -Force
+Remove-Item $ZipPath -Recurse -Force
+
+Write-Output "Restarting ADS"
+ampinstmgr -r ADS
