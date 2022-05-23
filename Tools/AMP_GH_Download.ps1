271dbba17a22a7805638795114502ad30267b172
James Manker <jamesmanker@pm.me>
2022-05-23 10:41:58 -0500

    Upgraded Logic
    Changed to use Regex for finding the right lines due to the output of ampinstmgr being different in Windows vs Linux. Made it robust so it doesn't matter what line the results come in on.

--- Tools/AMP_GH_Download.ps1
+++ Tools/AMP_GH_Download.ps1
@@ -1,20 +1,51 @@
-﻿[string]$ADS = ampinstmgr -i ADS | Select-String -Pattern "Data Path"
-$ADSPath = $ADS.Substring(21)
+﻿$List = ampinstmgr -l
+
+$ADSFound = ''
+$ADSPath = ''
+$InstanceName = ''
+$i = 0
+Foreach ($Line in $List)
+{
+    If ($Line -match 'Module.*ADS')
+    {
+        $ADSFound = 'Y'
+    }
+    
+    If ($ADSFound -eq 'Y' -and $Line -match 'Instance Name.*')
+    {
+        $InstanceName = $Line.Substring(21)
+    }
+
+    If ($ADSFound -eq 'Y' -and $Line -match 'Data Path.*')
+    {
+        $ADSPath = $Line.Substring(21)
+    }
+
+    If ($InstanceName -and $ADSPath)
+    {
+        break
+    }
+
+    $i++
+}
+
+
 $TemplatesPath = $ADSPath + "\Plugins\ADSModule\GenericTemplates"
 $ZipPath = $TemplatesPath + "\AMPTemplates-main"
 
-Write-Output "ADS Path $ADS"
+Write-Output "ADS Instance Name $InstanceName"
+Write-Output "ADS Path $ADSPath"
 Write-Output "Change directory to $TemplatesPath"
 cd "$TemplatesPath"
 
-Write-Output "Downloading AMPTemplates GH"
+Write-Output "Downloading AMPTemplates repo"
 Invoke-WebRequest 'https://github.com/CubeCoders/AMPTemplates/archive/refs/heads/main.zip' -OutFile .\amptemplates.zip
 
 Write-Output "Extracting downloaded zip file"
 Expand-Archive .\amptemplates.zip .\ -Force
 
 Write-Output "Deleting downloaded zip file"
-Remove-Item .\amptemplates.zip
+Remove-Item .\amptemplates.zip -Force
 
 Write-Output "Moving files into main template directory"
 Get-ChildItem -Path $ZipPath -Recurse -File | Move-Item -Destination $TemplatesPath -Force
@@ -23,5 +54,5 @@ Write-Output "Removing unnecessary items"
 Remove-Item $TemplatesPath\*.md -Force
 Remove-Item $ZipPath -Recurse -Force
 
-Write-Output "Restarting ADS"
-ampinstmgr -r ADS
+Write-Output "Restarting $InstanceName"
+ampinstmgr -r $InstanceName
