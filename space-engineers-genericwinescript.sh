0d901a182f768409fe94954fe89ed8d7b27f9873
Greelan <53196309+Greelan@users.noreply.github.com>
2023-04-18 18:57:57 +1000

    Updates

--- space-engineers-genericwinescript.sh
+++ space-engineers-genericwinescript.sh
@@ -2,7 +2,7 @@
 # Adapted with thanks from https://github.com/mmmaxwwwell/space-engineers-dedicated-docker-linux/blob/main/install-winetricks
 
 cd ./space-engineers-generic
-Xvfb :5 -screen 0 1024x768x16 &
+/usr/bin/Xvfb :5 -screen 0 1024x768x16 &
 env WINEPREFIX=./.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks corefonts 
 env DISPLAY=:5.0 WINEPREFIX=./.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks -q vcrun2017 
 env DISPLAY=:5.0 WINEPREFIX=./.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks -q --force dotnet48 
