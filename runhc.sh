ccf774cccc16129f9ac056cef66672748516bd11
Greelan <53196309+Greelan@users.noreply.github.com>
2023-05-02 14:20:09 +1000

    Script tweaks

--- runhc.sh
+++ runhc.sh
@@ -2,29 +2,22 @@
 
 # Arguments: <number_clients> <server_binding> <server_port> "<server_password>" "<mod_list>"
 
-# Function to check if a port is available
-function port_available() {
-  local lines
-  lines=$(netstat -uln | grep -c ":$1 ")
-  return "$lines"
-}
-
-# Check if no headless clients are to be run, and
-# if so immediately exit
+# Check if no headless clients are to be run
+# If none, immediately exit
 [[ $1 -eq 0 ]] && exit 0
 
 # Start the headless clients
+baseport=$(($3 + 498))
+netcommand=$(command -v "ss" &> /dev/null && echo "ss" || echo "netstat") 
+export LD_LIBRARY_PATH=`dirname $0`/linux64:$LD_LIBRARY_PATH
+cd ./arma3/233780
 for i in $(seq 1 $1); do
-  port=3300
-  while ! $(port_available $port); do
-    port=$((port+1))
-  done
   if [[ "$2" == "0.0.0.0" ]]; then
     connect="127.0.0.1"
   else
     connect="$2"
   fi
-  ./arma3/233780/arma3server -client -nosound -connect="$connect:$3" -port="$port" -password="$4" "-mod=$5" 2>&1 >/dev/null &
+  ./arma3server_x64 -client -nosound -connect="$connect:$3" -port="$baseport" -password="$4" "-mod=$5" &> /dev/null &
   clients+=($!)
 done
 
@@ -32,7 +25,7 @@ done
 # If not, terminate headless clients
 server_started=false
 for i in $(seq 1 180); do
-  if netstat -uln | grep -q ":$3 "; then
+  if $netcommand -uln | grep -q ":$3 "; then
     server_started=true
     break
   fi
@@ -41,18 +34,18 @@ done
 
 if ! $server_started; then
   for client in "${clients[@]}"; do
-    kill $client >/dev/null 2>&1
+    kill $client &> /dev/null
   done
   exit 1
 fi
 
 # Monitor server process and terminate headless clients
 # when server terminates
-trap 'kill "${clients[@]}" >/dev/null 2>&1' SIGTERM
+trap 'kill "${clients[@]}" &> /dev/null' SIGTERM
 while true; do
-  if ! netstat -uln | grep -q ":$3 "; then
+  if ! $netcommand -uln | grep -q ":$3 "; then
     for client in "${clients[@]}"; do
-      kill $client >/dev/null 2>&1
+      kill $client &> /dev/null
     done
     exit 0
   fi
