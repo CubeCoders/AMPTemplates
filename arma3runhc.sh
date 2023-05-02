8cac78ea56d869c3053ebf616a7bc38ccb78112a
Greelan <53196309+Greelan@users.noreply.github.com>
2023-05-02 16:22:32 +1000

    Update arma3runhc.sh

--- arma3runhc.sh
+++ arma3runhc.sh
@@ -2,13 +2,14 @@
 
 # Arguments: <number_clients> <server_binding> <server_port> "<server_password>" "<mod_list>"
 
+netcommand="$(command -v "ss" 2>&1 >/dev/null && echo "ss" || echo "netstat")"
+
 # Check if no headless clients are to be run
 # If none, immediately exit
 [[ $1 -eq 0 ]] && exit 0
 
 # Start the headless clients
 baseport=$(($3 + 498))
-netcommand="$(command -v "ss" &> /dev/null && echo "ss" || echo "netstat")"
 export LD_LIBRARY_PATH=`dirname $0`/linux64:$LD_LIBRARY_PATH
 cd ./arma3/233780
 for i in $(seq 1 $1); do
@@ -17,7 +18,7 @@ for i in $(seq 1 $1); do
   else
     connect="$2"
   fi
-  ./arma3server_x64 -client -nosound -connect="$connect:$3" -port="$baseport" -password="$4" "-mod=$5" &> /dev/null &
+  ./arma3server_x64 -client -nosound -connect="$connect:$3" -port="$baseport" -password="$4" "-mod=$5" 2>&1 >/dev/null &
   clients+=($!)
 done
 
@@ -34,18 +35,18 @@ done
 
 if ! $server_started; then
   for client in "${clients[@]}"; do
-    kill $client &> /dev/null
+    kill $client 2>&1 >/dev/null
   done
   exit 1
 fi
 
 # Monitor server process and terminate headless clients
 # when server terminates
-trap 'kill "${clients[@]}" &> /dev/null' SIGTERM
+trap 'kill "${clients[@]}" 2>&1 >/dev/null' SIGTERM
 while true; do
   if ! $netcommand -uln | grep -q ":$3 "; then
     for client in "${clients[@]}"; do
-      kill $client &> /dev/null
+      kill $client 2>&1 >/dev/null
     done
     exit 0
   fi
