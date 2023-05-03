fedc09f537b65390f88cff5267e782d4fc477e29
Greelan <53196309+Greelan@users.noreply.github.com>
2023-05-03 19:17:05 +1000

    Update arma3runhc.bat

--- arma3runhc.bat
+++ arma3runhc.bat
@@ -18,7 +18,7 @@ for /l %%i in (1,1,%1) do (
     set "connect=%2"
   )
   start /b "hc%%i" "ArmA3Server_x64.exe" -client -nosound -connect="%connect%:%3" -port="%baseport%" -password="%4" "-mod=%5" >nul 2>&1
-  for /f "tokens=2" %%a in ('tasklist /v /fi "WindowTitle eq hc%%i" /fo list /nh') do set "clients=%clients% %%a"
+  for /f "tokens=2" %%a in ('tasklist /v /fi "WindowTitle eq hc%%i" /fo list') do set "clients=%clients% %%a"
 )
 
 rem Check if server starts successfully within 3 minutes
