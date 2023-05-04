6eb03403501835b34803bf68e879fe00e225c5d4
Greelan <53196309+Greelan@users.noreply.github.com>
2023-05-04 16:47:15 +1000

    Update arma3runhc.ps1

--- arma3runhc.ps1
+++ arma3runhc.ps1
@@ -1,12 +1,11 @@
-Arguments: [number_clients] [server_binding] [server_port] "<server_password>" "<mod_list>"
+# Arguments: [number_clients] [server_binding] [server_port] "<server_password>" "<mod_list>"
 
-$clients = ""
-
-Check if any headless clients are to be run
-If none, immediately exit
+# Check if any headless clients are to be run
+# If none, immediately exit
 if ($args[0] -eq "0") { exit }
 
-Start the headless clients
+# Start the headless clients
+$clients = ""
 $basePort = [int]$args[2] + 498
 cd "$PSScriptRoot\arma3\233780"
 for ($i = 1; $i -le [int]$args[0]; $i++) {
@@ -15,13 +14,12 @@ for ($i = 1; $i -le [int]$args[0]; $i++) {
   } else {
     $connect = $args[1]
   }
-  $hcProcess = Start-Process -FilePath "ArmA3Server_x64.exe" -ArgumentList "-client", "-nosound", "-connect="$connect:$($args[2])"", "-port="$basePort"", "-password="$($args[3])"", ""-mod=$($args[4])`"" -WindowStyle Hidden -PassThru
-  $hcProcess.Id
-  $clients += " $hcProcess.Id"
+  $hcProcess = Start-Process -FilePath "ArmA3Server_x64.exe" -ArgumentList "-client", "-nosound", "-connect=${connect}:$($args[2])", "-port=$basePort", "-password=`"$($args[3])`"", "`"-mod=$($args[4])`"" -WindowStyle Hidden -PassThru
+  $clients += " $($hcProcess.Id)"
 }
 
-Check if server starts successfully within 3 minutes
-If not, terminate headless clients
+# Check if server starts successfully within 3 minutes
+# If not, terminate headless clients
 $serverStarted = $false
 for ($i = 1; $i -le 180; $i++) {
   if (Get-NetUDPEndpoint -LocalPort $args[2] -ErrorAction SilentlyContinue) {
@@ -32,17 +30,17 @@ for ($i = 1; $i -le 180; $i++) {
 }
 if (-not $serverStarted) {
   foreach ($processId in $clients.Trim().Split(" ")) {
-    Stop-Process -Id $processId -Force
+    Stop-Process -Id ([int]$processId) -Force
   }
   exit 1
 }
 
-Monitor server process and terminate headless clients
-when server terminates
+# Monitor server process and terminate headless clients
+# when server terminates
 while ($true) {
   if (-not (Get-NetUDPEndpoint -LocalPort $args[2] -ErrorAction SilentlyContinue)) {
     foreach ($processId in $clients.Trim().Split(" ")) {
-      Stop-Process -Id $processId -Force
+      Stop-Process -Id ([int]$processId) -Force
     }
     exit 0
   }
