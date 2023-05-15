58e7f453d8bc6c85643c1ad727eb0030455ad909
Greelan <53196309+Greelan@users.noreply.github.com>
2023-05-15 10:26:50 +1000

    Clean up

--- dayz-experimentalmodscript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find ./dayz/1042420/steamapps/workshop/content/221100/ -type f -name "*[A-Z]*" -print0 |\
- xargs -0 -I {} bash -c "mv \"{}\" \"\`echo \"{}\" | sed 's,\(.*\)\/\(.*\),\1\/\L\2,'\`\"";\
- cd ./dayz/1042420 &&\
- find ./steamapps/workshop/content/221100 -maxdepth 1 -mindepth 1 -type d -exec ln -sf -t ./ {} +
-exit 0
