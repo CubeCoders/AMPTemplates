8090c9ed577475cec484f21986376c6fd6494b04
Greelan <53196309+Greelan@users.noreply.github.com>
2023-05-17 13:22:19 +1000

    Update space-engineers-genericwinescript.sh

--- space-engineers-genericwinescript.sh
+++ space-engineers-genericwinescript.sh
@@ -5,11 +5,11 @@ SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 SCRIPTDIR=$(dirname "$SCRIPT_NAME")
 cd ./space-engineers-generic
 /usr/bin/Xvfb :5 -screen 0 1024x768x16 &
-env WINEPREFIX=$SCRIPTDIR/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks corefonts 
-env DISPLAY=:5.0 WINEPREFIX=$SCRIPTDIR/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks -q vcrun2017 
-env DISPLAY=:5.0 WINEPREFIX=$SCRIPTDIR/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks -q --force dotnet48 
-env WINEPREFIX=$SCRIPTDIR/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks sound=disabled 
-env DISPLAY=:5.0 WINEPREFIX=$SCRIPTDIR/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks -q vcrun2013 
+env WINEPREFIX=$SCRIPTDIR/space-engineers-generic/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks corefonts 
+env DISPLAY=:5.0 WINEPREFIX=$SCRIPTDIR/space-engineers-generic/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks -q vcrun2017 
+env DISPLAY=:5.0 WINEPREFIX=$SCRIPTDIR/space-engineers-generic/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks -q --force dotnet48 
+env WINEPREFIX=$SCRIPTDIR/space-engineers-generic/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks sound=disabled 
+env DISPLAY=:5.0 WINEPREFIX=$SCRIPTDIR/space-engineers-generic/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks -q vcrun2013 
 rm -rf ~/.cache/winetricks ~/.cache/fontconfig
 
 exit 0
