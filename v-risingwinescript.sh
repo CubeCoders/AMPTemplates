3634393baa23d6c10ef1cb39403bdab3b7603616
Greelan <53196309+Greelan@users.noreply.github.com>
2023-06-18 08:26:08 +1000

    Create v-risingwinescript.sh

---
+++ v-risingwinescript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+SCRIPTDIR=$(dirname "$SCRIPT_NAME")
+/usr/bin/Xvfb :5 -screen 0 1024x768x16 -ac -nolisten tcp -nolisten unix &
+xvfb_pid=$!
+wget -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
+export WINEPREFIX=$SCRIPTDIR/.wine
+export DISPLAY=:5
+./winetricks -q --force dotnet6
+rm -rf ~/.cache/winetricks
+kill $xvfb_pid
+
+exit 0
