c4ef3b710a58f631650ceafe27d19f2006b595e1
Greelan <53196309+Greelan@users.noreply.github.com>
2023-06-18 16:07:18 +1000

    Update space-engineers-genericwinescript.sh

--- space-engineers-genericwinescript.sh
+++ space-engineers-genericwinescript.sh
@@ -9,11 +9,12 @@ chmod +x winetricks
 export WINEPREFIX=$SCRIPTDIR/space-engineers-generic/.wine
 export WINEDLLOVERRIDES="mscoree,mshtml="
 export WINEARCH=win64
-export DISPLAY=:5
-./winetricks -q corefonts > winescript_log.txt 2>&1
-./winetricks -q vcrun2017 >> winescript_log.txt 2>&1
-./winetricks -q --force dotnet48 >> winescript_log.txt 2>&1
+export DISPLAY=:5.0
+/usr/bin/wine wincfg /v win10 > winescript_log.txt 2>&1
+./winetricks corefonts >> winescript_log.txt 2>&1
 ./winetricks sound=disabled >> winescript_log.txt 2>&1
+./winetricks -q vcrun2022 >> winescript_log.txt 2>&1
+./winetricks -q --force dotnet48 >> winescript_log.txt 2>&1
 #./winetricks -q vcrun2013 >> winescript_log.txt 2>&1
 rm -rf ~/.cache/winetricks ~/.cache/fontconfig
 kill $xvfb_pid
