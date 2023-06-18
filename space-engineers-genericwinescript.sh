83cbdb0fea808b063dabf4eef678b18fd31c2273
Greelan <53196309+Greelan@users.noreply.github.com>
2023-06-18 11:12:03 +1000

    Update space-engineers-genericwinescript.sh

--- space-engineers-genericwinescript.sh
+++ space-engineers-genericwinescript.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-# Adapted with thanks from https://github.com/mmmaxwwwell/space-engineers-dedicated-docker-linux/blob/main/install-winetricks
 
 SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 SCRIPTDIR=$(dirname "$SCRIPT_NAME")
-cd ./space-engineers-generic
-/usr/bin/Xvfb :5 -screen 0 1024x768x16 &
-env WINEPREFIX=$SCRIPTDIR/space-engineers-generic/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks corefonts 
-env DISPLAY=:5.0 WINEPREFIX=$SCRIPTDIR/space-engineers-generic/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks -q vcrun2017 
-env DISPLAY=:5.0 WINEPREFIX=$SCRIPTDIR/space-engineers-generic/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks -q --force dotnet48 
-env WINEPREFIX=$SCRIPTDIR/space-engineers-generic/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks sound=disabled 
-env DISPLAY=:5.0 WINEPREFIX=$SCRIPTDIR/space-engineers-generic/.wine WINEARCH=win64 WINEDEBUG=-all ./winetricks -q vcrun2013 
+/usr/bin/Xvfb :5 -screen 0 1024x768x16 -ac -nolisten tcp -nolisten unix &
+xvfb_pid=$!
+wget -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
+export WINEPREFIX=$SCRIPTDIR/space-engineers-generic/.wine
+export WINEDLLOVERRIDES="mscoree,mshtml="
+export WINEARCH=win64
+export DISPLAY=:5
+./winetricks -q corefonts > winescript_log.txt 2>&1
+./winetricks -q vcrun2017 >> winescript_log.txt 2>&1
+./winetricks -q --force dotnet48 >> winescript_log.txt 2>&1
+./winetricks sound=disabled
+./winetricks -q vcrun2013 >> winescript_log.txt 2>&1
 rm -rf ~/.cache/winetricks ~/.cache/fontconfig
+kill $xvfb_pid
 
 exit 0
