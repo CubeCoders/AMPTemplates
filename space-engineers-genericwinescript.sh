44fd1b697dfbd51a446d9fe3d5b86a1d85a1018a
Greelan <53196309+Greelan@users.noreply.github.com>
2023-06-18 21:07:58 +1000

    Update space-engineers-genericwinescript.sh

--- space-engineers-genericwinescript.sh
+++ space-engineers-genericwinescript.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 SCRIPTDIR=$(dirname "$SCRIPT_NAME")
-/usr/bin/Xvfb :5 -screen 0 1024x768x16 -ac -nolisten tcp -nolisten unix &
+/usr/bin/Xvfb :5 -screen 0 1024x768x16 &
 xvfb_pid=$!
 wget -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
 chmod +x winetricks
