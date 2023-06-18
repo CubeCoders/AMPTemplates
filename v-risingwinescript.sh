c3b28f20ae5391f1f00aef0c9c7a2f81fe24d8d1
Greelan <53196309+Greelan@users.noreply.github.com>
2023-06-18 19:45:10 +1000

    Update v-risingwinescript.sh

--- v-risingwinescript.sh
+++ v-risingwinescript.sh
@@ -11,7 +11,7 @@ export WINEDLLOVERRIDES="mscoree,mshtml="
 export WINEARCH=win64
 export DISPLAY=:5.0
 ./winetricks -q vcrun2019 > winescript_log.txt 2>&1
-#./winetricks -q --force dotnet48 >> winescript_log.txt 2>&1
+./winetricks -q dotnet48 >> winescript_log.txt 2>&1
 ./winetricks -q dotnet6 >> winescript_log.txt 2>&1
 rm -rf ~/.cache/winetricks
 kill $xvfb_pid
