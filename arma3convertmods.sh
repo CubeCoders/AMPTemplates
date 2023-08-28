699c81adba3f20b59faa6b746d486169cd4979b9
JonathanArendt <arendtjda@gmail.com>
2023-08-27 23:08:02 -0500

    Added Draft for Stormworks

--- arma3convertmods.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find ./arma3/233780/steamapps/workshop/content/107410/ -depth -name "*[A-Z]*" -print0 |\
- xargs -0 -I {} bash -c "mv \"{}\" \"\`echo \"{}\" | sed 's,\(.*\)\/\(.*\),\1\/\L\2,'\`\""
-exit 0
