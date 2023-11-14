40baa7c52fb6b6d123123880ab1fe1a243575524
Daniel Elliott <ssfdre38@msn.com>
2023-11-14 13:41:07 -0800

    clean dev space

--- dayz-experimentalconvertmods.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find ./dayz/1042420/steamapps/workshop/content/221100/ -depth -name "*[A-Z]*" -print0 |\
- xargs -0 -I {} bash -c "mv \"{}\" \"\`echo \"{}\" | sed 's,\(.*\)\/\(.*\),\1\/\L\2,'\`\""
-exit 0
