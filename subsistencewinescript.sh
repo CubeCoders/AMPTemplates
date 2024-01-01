333e4e3433ea52096b1c6e66fef1a590a5899482
Greelan <53196309+Greelan@users.noreply.github.com>
2024-01-01 18:44:31 +1100

    Subsistence

---
+++ subsistencewinescript.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+SCRIPTDIR=$(dirname "$SCRIPT_NAME")
+
+exec 6>display.log
+/usr/bin/Xvfb -displayfd 6 &
+XVFB_PID=$!
+while [[ ! -s display.log ]]; do
+  sleep 1
+done
+read -r DPY_NUM < display.log
+rm display.log
+
+export WINEPREFIX="$SCRIPTDIR/subsistence/.wine"
+export WINEDLLOVERRIDES="mscoree,mshtml="
+export WINEARCH=win64
+export WINEDEBUG=fixme-all
+export DISPLAY=:$DPY_NUM
+
+wget -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
+
+./winetricks -q vcrun2010 > winescript_log.txt 2>&1
+./winetricks -q vcrun2019 >> winescript_log.txt 2>&1
+./winetricks -q d3dx9 >> winescript_log.txt 2>&1
+
+rm -rf ~/.cache/winetricks
+
+exec 6>&-
+kill $XVFB_PID
+
+exit 0
