7f33e6ee5cd69dd2622da31e911f3473018361d8
Greelan <53196309+Greelan@users.noreply.github.com>
2024-02-08 12:47:58 +1100

    Update space-station14server_download.ps1

--- space-station14server_download.ps1
+++ space-station14server_download.ps1
@@ -1,7 +1,7 @@
 cd "$PSScriptRoot\space-station14\server"
 
 # Download the manifest JSON file and convert it to a PowerShell object
-$Manifest = Invoke-WebRequest -Uri 'https://cdn.centcomm.spacestation14.com/builds/wizards/manifest.json' | ConvertFrom-Json
+$Manifest = Invoke-WebRequest -UseBasicParsing -Uri 'https://cdn.centcomm.spacestation14.com/builds/wizards/manifest.json' | ConvertFrom-Json
 
 # Find the last build key
 $LastBuildKey = ($Manifest.builds | Get-Member -MemberType NoteProperty | Select-Object -Last 1).Name
@@ -10,6 +10,6 @@ $LastBuildKey = ($Manifest.builds | Get-Member -MemberType NoteProperty | Select
 $DownloadUrl = $Manifest.builds.$LastBuildKey.server.'win-x64'.url
 
 # Download, extract and clean up
-Invoke-WebRequest -Uri $DownloadUrl -OutFile "SS14.Server_win-x64.zip"
+Invoke-WebRequest -UseBasicParsing -Uri $DownloadUrl -OutFile "SS14.Server_win-x64.zip"
 Expand-Archive -Path "SS14.Server_win-x64.zip" -DestinationPath . -Force
 Remove-Item "SS14.Server_win-x64.zip"
