895bea16b709df7a14a6479980b68f1c600189a9
Greelan <53196309+Greelan@users.noreply.github.com>
2024-04-14 10:08:05 +1000

    Arma 3 and DayZ: more console suppression

--- arma3modscript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find ./arma3/233780/steamapps/workshop/content/107410/ -depth -name "*[A-Z]*" -print0 |\
- xargs -0 -I {} bash -c "mv \"{}\" \"\`echo \"{}\" | sed 's,\(.*\)\/\(.*\),\1\/\L\2,'\`\"";\
- cd ./arma3/233780 &&\
- find ./steamapps/workshop/content/107410 -maxdepth 1 -mindepth 1 -type d -exec ln -sf -t ./ {} +
-exit 0
