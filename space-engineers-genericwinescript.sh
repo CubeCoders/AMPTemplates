dd1275b0982b276b20d94c142d5d2c390fe251df
Greelan <53196309+Greelan@users.noreply.github.com>
2024-05-21 17:48:36 +1000

    Update space-engineers-genericwinescript.sh

--- space-engineers-genericwinescript.sh
+++ space-engineers-genericwinescript.sh
@@ -4,7 +4,7 @@ SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 SCRIPTDIR=$(dirname "$SCRIPT_NAME")
 
 exec 6>display.log
-/usr/bin/Xvfb -nolisten tcp -nolisten unix -displayfd 6 &
+/usr/bin/Xvfb -displayfd 6 -nolisten tcp -nolisten unix &
 XVFB_PID=$!
 while [[ ! -s display.log ]]; do
   sleep 1
@@ -12,21 +12,19 @@ done
 read -r DPY_NUM < display.log
 rm display.log
 
+wget -q -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
+
 export WINEPREFIX="$SCRIPTDIR/space-engineers-generic/.wine"
 export WINEDLLOVERRIDES="mscoree,mshtml="
 export WINEARCH=win64
 export WINEDEBUG=fixme-all
 export DISPLAY=:$DPY_NUM
-
-wget -q -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
-chmod +x winetricks
-wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/9.0.0/wine-mono-9.0.0-x86.msi
-
-./winetricks -q win11 > winescript_log.txt 2>&1
-./winetricks -q vcrun2022 >> winescript_log.txt 2>&1
-/usr/bin/wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
-./winetricks -q corefonts >> winescript_log.txt 2>&1
-./winetricks -q sound=disabled >> winescript_log.txt 2>&1
+./winetricks corefonts > winescript_log.txt 2>&1
+./winetricks sound=disabled >> winescript_log.txt 2>&1
+./winetricks -q vcrun2013 >> winescript_log.txt 2>&1
+./winetricks -q vcrun2019 >> winescript_log.txt 2>&1
+./winetricks -q dotnet48 >> winescript_log.txt 2>&1
 rm -rf ~/.cache/winetricks ~/.cache/fontconfig
 
 exec 6>&-
