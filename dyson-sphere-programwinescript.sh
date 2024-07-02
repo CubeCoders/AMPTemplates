63374f4c913ddb3ac64d1e0744c2dc07d2cd543e
Greelan <53196309+Greelan@users.noreply.github.com>
2024-07-02 16:52:33 +1000

    DSP

---
+++ dyson-sphere-programwinescript.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+SCRIPTDIR=$(dirname "$SCRIPT_NAME")
+
+exec 6>display.log
+/usr/bin/Xvfb -displayfd 6 -nolisten tcp -nolisten unix &
+XVFB_PID=$!
+while [[ ! -s display.log ]]; do
+  sleep 1
+done
+read -r DPY_NUM < display.log
+rm display.log
+
+export WINEPREFIX="$SCRIPTDIR/dsp/.wine"
+export WINEDLLOVERRIDES="mscoree,mshtml="
+export WINEARCH=win64
+export WINEDEBUG=fixme-all
+export DISPLAY=:$DPY_NUM
+
+wget -q -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
+wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/8.0.0/wine-mono-8.0.0-x86.msi
+
+/usr/bin/wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
+./winetricks -q vcrun2022 > winescript_log.txt 2>&1
+./winetricks -q sound-disabled >> winescript_log.txt 2>&1
+rm -rf ~/.cache/winetricks
+
+exec 6>&-
+kill $XVFB_PID
+
+exit 0
