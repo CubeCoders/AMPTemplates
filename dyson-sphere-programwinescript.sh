a3a391411f58d398b28815743fcb076865ad26fe
Greelan <53196309+Greelan@users.noreply.github.com>
2024-07-02 17:25:09 +1000

    DSP

--- dyson-sphere-programwinescript.sh
+++ dyson-sphere-programwinescript.sh
@@ -20,12 +20,13 @@ export DISPLAY=:$DPY_NUM
 
 wget -q -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
 chmod +x winetricks
-wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/8.0.0/wine-mono-8.0.0-x86.msi
 
-/usr/bin/wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
-./winetricks -q vcrun2022 > winescript_log.txt 2>&1
-./winetricks -q sound-disabled >> winescript_log.txt 2>&1
-rm -rf ~/.cache/winetricks
+PACKAGES="vcrun2013 vcrun2017 corefonts dotnet48 sound=disabled"
+echo "" > winescript_log.txt 2>&1
+for PACKAGE in $PACKAGES; do
+  ./winetricks -q $PACKAGE >> winescript_log.txt 2>&1
+done
+rm -rf ~/.cache/winetricks ~/.cache/fontconfig
 
 exec 6>&-
 kill $XVFB_PID
