608d19b8903b3480ce671e08b638ad899e8ed6b6
Greelan <53196309+Greelan@users.noreply.github.com>
2025-01-21 13:32:02 +1100

    Update v-risingwinescript.sh

--- v-risingwinescript.sh
+++ v-risingwinescript.sh
@@ -18,7 +18,7 @@ export WINEARCH=win64
 export WINEDEBUG=fixme-all
 export DISPLAY=:$DPY_NUM
 
-wget -q -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+wget -q -N https://raw.githubusercontent.com/Winetricks/winetricks/refs/tags/20250102/src/winetricks
 chmod +x winetricks
 wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/8.0.0/wine-mono-8.0.0-x86.msi
 
