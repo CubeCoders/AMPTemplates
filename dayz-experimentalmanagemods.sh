286097590edc6164cce3f9531a4dae618d18e123
Greelan <53196309+Greelan@users.noreply.github.com>
2025-01-22 07:23:42 +1100

    Update dayz-experimentalmanagemods.sh

--- dayz-experimentalmanagemods.sh
+++ dayz-experimentalmanagemods.sh
@@ -4,26 +4,28 @@ ModDirFormat="$1"
 
 cd ./dayz/1042420
 
-if [ -d ./steamapps/workshop/content/221100 ]; then
+workshopDir="./steamapps/workshop/content/221100"
+
+if [ -d $workshopDir ]; then
   # Convert uppercase filenames to lowercase
-  find ./steamapps/workshop/content/221100/ -depth -name "*[A-Z]*" -print0 |\
+  find $workshopDir/ -depth -name "*[A-Z]*" -print0 |\
   xargs -0 -I {} bash -c "mv \"{}\" \"\$(echo \"{}\" | sed 's,\(.*\)\/\(.*\),\1\/\L\2,')\"" >/dev/null 2>&1
 
   if [ "$ModDirFormat" = "false" ]; then
     # Remove @name symlinks corresponding to the mod directories based on meta.cpp
-    find ./steamapps/workshop/content/221100 -maxdepth 1 -mindepth 1 -type d | while read -r mod_dir; do
+    find $workshopDir -maxdepth 1 -mindepth 1 -type d | while read -r mod_dir; do
       mod_name=$(sed -n 's/^[[:space:]]*name[[:space:]]*=[[:space:]]*"\([^"]\+\)".*/\1/p' "$mod_dir/meta.cpp")
       [[ -n "$mod_name" ]] && rm -f "./@$mod_name" >/dev/null 2>&1
     done
     # Create numbered symlinks for the mod directories
-    find ./steamapps/workshop/content/221100 -maxdepth 1 -mindepth 1 -type d -exec ln -sf {} ./ \;
+    find $workshopDir -maxdepth 1 -mindepth 1 -type d -exec ln -sf {} ./ \;
   else
     # Remove numbered symlinks for the mod directories
-    find ./steamapps/workshop/content/221100 -maxdepth 1 -mindepth 1 -type d | while read -r mod_dir; do
+    find $workshopDir -maxdepth 1 -mindepth 1 -type d | while read -r mod_dir; do
       rm -f "./$(basename "$mod_dir")" >/dev/null 2>&1
     done
     # Create @name symlinks corresponding to the mod directories based on meta.cpp
-    find ./steamapps/workshop/content/221100 -maxdepth 1 -mindepth 1 -type d | while read -r mod_dir; do
+    find $workshopDir -maxdepth 1 -mindepth 1 -type d | while read -r mod_dir; do
       mod_name=$(sed -n 's/^[[:space:]]*name[[:space:]]*=[[:space:]]*"\([^"]\+\)".*/\1/p' "$mod_dir/meta.cpp")
       [[ -n "$mod_name" ]] && ln -sfT "$mod_dir" "@$mod_name"
     done
