8d6e048fa5d845aa33f01c0c2d8041b9c1da3d2f
Greelan <53196309+Greelan@users.noreply.github.com>
2025-01-22 07:50:35 +1100

    Update dayz-experimentalmanagemods.ps1

--- dayz-experimentalmanagemods.ps1
+++ dayz-experimentalmanagemods.ps1
@@ -5,10 +5,12 @@ Set-Location -Path ".\dayz\1042420"
 $workshopDir = ".\steamapps\workshop\content\221100"
 
 if (Test-Path $workshopDir) {
-    if ($ModDirFormat -eq "false") {
-        Get-ChildItem -Path $workshopDir -Directory | ForEach-Object {
-            $modDir = Join-Path -Path $workshopDir -ChildPath $_.Name
-            $modName = (Select-String -Path "$modDir\meta.cpp" -Pattern '^\s*name\s*=\s*"(.*)"' -AllMatches).Matches.Groups[1].Value
+    Get-ChildItem -Path $workshopDir -Directory | ForEach-Object {
+        # Extract modName from meta.cpp
+        $modDir = Join-Path -Path $workshopDir -ChildPath $_.Name
+        $modName = (Select-String -Path "$modDir\meta.cpp" -Pattern '^\s*name\s*=\s*"(.*)"' -AllMatches).Matches.Groups[1].Value
+
+        if ($ModDirFormat -eq "false") {
             # Remove @name junction links corresponding to the mod directories based on meta.cpp
             if ($modName) {
                 $symlinkName = "@$modName"
@@ -22,17 +24,12 @@ if (Test-Path $workshopDir) {
             }
             New-Item -ItemType Junction -Name $_.Name -Target $modDir -Force | Out-Null
         }
-    }
-    else {
-
-        Get-ChildItem -Path $workshopDir -Directory | ForEach-Object {
+        else {
             # Remove numbered junction links for the mod directories
             if (Test-Path $_.Name) {
                 Remove-Item -Path $_.Name -Force -Recurse
             }
             # Create @name junction links for the mod directories based on meta.cpp
-            $modDir = Join-Path -Path $workshopDir -ChildPath $_.Name
-            $modName = (Select-String -Path "$modDir\meta.cpp" -Pattern '^\s*name\s*=\s*"(.*)"' -AllMatches).Matches.Groups[1].Value
             if ($modName) {
                 $symlinkName = "@$modName"
                 if (Test-Path $symlinkName) {
