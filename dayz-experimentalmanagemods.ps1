0b1b74eb40ec71fc914af9bf3e61e818bf67e951
Greelan <53196309+Greelan@users.noreply.github.com>
2025-01-22 07:17:13 +1100

    Update dayz-experimentalmanagemods.ps1

--- dayz-experimentalmanagemods.ps1
+++ dayz-experimentalmanagemods.ps1
@@ -6,35 +6,33 @@ $workshopDir = ".\steamapps\workshop\content\221100"
 
 if (Test-Path $workshopDir) {
     if ($ModDirFormat -eq "false") {
-        # Remove @name junction links corresponding to the mod directories based on meta.cpp
         Get-ChildItem -Path $workshopDir -Directory | ForEach-Object {
-            $modDir = $_.FullName
+            $modDir = Join-Path -Path $workshopDir -ChildPath $_.Name
             $modName = (Select-String -Path "$modDir\meta.cpp" -Pattern '^\s*name\s*=\s*"(.*)"' -AllMatches).Matches.Groups[1].Value
+            # Remove @name junction links corresponding to the mod directories based on meta.cpp
             if ($modName) {
                 $symlinkName = "@$modName"
                 if (Test-Path $symlinkName) {
                     Remove-Item -Path $symlinkName -Force -Recurse
                 }
             }
-        }
-        # Create numbered junction links for the mod directories
-        Get-ChildItem -Path $workshopDir -Directory | ForEach-Object {
+            # Create numbered junction links for the mod directories
             if (Test-Path $_.Name) {
                 Remove-Item -Path $_.Name -Force -Recurse
             }
-            New-Item -ItemType Junction -Name $_.Name -Target $_.FullName -Force | Out-Null
+            New-Item -ItemType Junction -Name $_.Name -Target $modDir -Force | Out-Null
         }
     }
     else {
-        # Remove numbered junction links for the mod directories
+
         Get-ChildItem -Path $workshopDir -Directory | ForEach-Object {
+            # Remove numbered junction links for the mod directories
             if (Test-Path $_.Name) {
                 Remove-Item -Path $_.Name -Force -Recurse
             }
-        }
-        # Create @name junction links for the mod directories based on meta.cpp
-        Get-ChildItem -Path $workshopDir -Directory | ForEach-Object {
-            $modDir = $_.FullName
+
+            # Create @name junction links for the mod directories based on meta.cpp
+            $modDir = Join-Path -Path $workshopDir -ChildPath $_.Name
             $modName = (Select-String -Path "$modDir\meta.cpp" -Pattern '^\s*name\s*=\s*"(.*)"' -AllMatches).Matches.Groups[1].Value
             if ($modName) {
                 $symlinkName = "@$modName"
