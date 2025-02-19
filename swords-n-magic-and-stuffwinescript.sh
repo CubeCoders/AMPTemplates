cdc9d8815d2b1a1356beccc9ec382330de1f5fa5
Greelan <53196309+Greelan@users.noreply.github.com>
2025-02-19 18:40:39 +1100

    SNMS: fix Linux

---
+++ swords-n-magic-and-stuffwinescript.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+SCRIPTDIR=$(dirname "$SCRIPT_NAME")
+
+exec 6>display.log
+/usr/bin/Xvfb -displayfd 6 -nolisten tcp -nolisten unix &
+XVFB_PID=$!
+while [[ ! -s display.log ]]; do
+  sleep 1
+done
+read -r DPY_NUM < display.log
+rm display.log
+
+export WINEPREFIX="$SCRIPTDIR/swords-n-magic-and-stuff/.wine"
+export WINEDLLOVERRIDES="mscoree,mshtml="
+export WINEARCH=win64
+export WINEDEBUG=fixme-all
+export DISPLAY=:$DPY_NUM
+
+[[ -f winetricks ]] && rm -f winetricks
+wget -q https://raw.githubusercontent.com/Winetricks/winetricks/refs/tags/20250102/src/winetricks
+chmod +x winetricks
+
+PACKAGES="vcrun2022"
+echo "" > winescript_log.txt 2>&1
+for PACKAGE in $PACKAGES; do
+  ./winetricks -q $PACKAGE >> winescript_log.txt 2>&1
+done
+rm -rf ~/.cache/winetricks
+
+exec 6>&-
+kill $XVFB_PID
+
+exit 0
