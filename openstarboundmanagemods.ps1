da1530293c8b4b8b8ea2a3a5e66ce1cf62f27e23
Greelan <53196309+Greelan@users.noreply.github.com>
2025-03-27 17:51:29 +1100

    Update openstarboundmanagemods.ps1

--- openstarboundmanagemods.ps1
+++ openstarboundmanagemods.ps1
@@ -3,26 +3,26 @@ Set-Location -Path "./openstarbound/server/mods"
 $workshopDir = "../../211820/steamapps/workshop/content/211820"
 
 if (Test-Path $workshopDir) {
-  Write-Output "Linking mods"
+  Write-Output "Copying mods"
 
   Get-ChildItem -Path $workshopDir -Directory | ForEach-Object {
     $modID = $_.Name
     $pakFile = Join-Path -Path $workshopDir -ChildPath "$modID/contents.pak"
 
     if (Test-Path $pakFile) {
-      $linkPath = "./$modID.pak"
+      $copyPath = "./$modID.pak"
 
-      if (Test-Path -LiteralPath $linkPath) {
-        Remove-Item -LiteralPath $linkPath -Force -Recurse
+      if (Test-Path -LiteralPath $copyPath) {
+        Remove-Item -LiteralPath $copyPath -Force
       }
 
-      New-Item -ItemType Junction -Path $linkPath -Target $pakFile -Force | Out-Null
+      Copy-Item -Path $pakFile -Destination $copyPath -Force | Out-Null
     } else {
       Write-Output "No contents.pak in $modID, skipping"
     }
   }
 } else {
-  Write-Output "No mods to link"
+  Write-Output "No mods to copy"
 }
 
 exit 0
