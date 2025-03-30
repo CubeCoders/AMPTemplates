e2564419c2102b7f4b1dc3f98a8f404977b44549
Greelan <53196309+Greelan@users.noreply.github.com>
2025-03-30 15:30:47 +1100

    Add OpenStarbound (#1384)

---
+++ openstarboundmanagemods.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd ./openstarbound/server/mods
+
+workshopDir="../../211820/steamapps/workshop/content/211820"
+
+if [ -d "$workshopDir" ]; then
+  echo "Linking mods"
+
+  find "$workshopDir" -maxdepth 1 -mindepth 1 -type d | while read -r modDir; do
+    modID=$(basename "$modDir")
+    pakFile="$modDir/contents.pak"
+
+    if [ -f "$pakFile" ]; then
+      ln -sf "$pakFile" "./$modID.pak"
+    else
+      echo "No contents.pak in $modID, skipping"
+    fi
+  done
+else
+  echo "No mods to link"
+fi
+
+exit 0
