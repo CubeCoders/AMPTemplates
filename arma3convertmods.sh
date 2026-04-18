37ca925aba4792f3189a736ff3ffcfbdea197237
Smurre <rale_95@live.se>
2026-04-18 11:40:34 +0000

    removed unnessesary games

--- arma3convertmods.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-[ -d ./arma3/233780/steamapps/workshop/content/107410 ] &&\
- find ./arma3/233780/steamapps/workshop/content/107410/ -depth -name "*[A-Z]*" -print0 |\
- xargs -0 -I {} bash -c "mv \"{}\" \"\`echo \"{}\" | sed 's,\(.*\)\/\(.*\),\1\/\L\2,'\`\"" >/dev/null 2>&1
-exit 0
--- dayz-experimentalconvertmods.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-[ -d ./dayz/1042420/steamapps/workshop/content/221100 ] &&\
- find ./dayz/1042420/steamapps/workshop/content/221100/ -depth -name "*[A-Z]*" -print0 |\
- xargs -0 -I {} bash -c "mv \"{}\" \"\`echo \"{}\" | sed 's,\(.*\)\/\(.*\),\1\/\L\2,'\`\"" >/dev/null 2>&1
-exit 0
