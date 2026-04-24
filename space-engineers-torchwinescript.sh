1262ddfb9c66171726938485c82761e90acfe7b4
Greelan <53196309+Greelan@users.noreply.github.com>
2026-04-24 16:28:53 +1000

    Wine prefix adjustments (#1891)

--- space-engineers-torchwinescript.sh
+++ space-engineers-torchwinescript.sh
@@ -23,12 +23,17 @@ export XDG_RUNTIME_DIR=/tmp
 wget -q https://raw.githubusercontent.com/Winetricks/winetricks/refs/tags/20260125/src/winetricks
 chmod +x winetricks
 
-PACKAGES="vcrun2022 corefonts dotnet48"
+PACKAGES="corefonts vcrun2022 dotnet48"
 echo "" > winescript_log.txt 2>&1
 for PACKAGE in $PACKAGES; do
   ./winetricks -q $PACKAGE >> winescript_log.txt 2>&1
 done
 rm -rf ~/.cache/winetricks ~/.cache/fontconfig
+
+# Set Wine prefix to Windows 10 and use GDI renderer for Direct3D: see https://github.com/OwendB1/Linux-SE-Tools/blob/production/torch-wrapper
+/usr/bin/wine reg add "HKCU\\Software\\Wine\\Version" /v Version /t REG_SZ /d win10 /f >/dev/null
+/usr/bin/wine reg add "HKCU\\Software\\Wine\\AppDefaults\\steamcmd.exe" /v Version /t REG_SZ /d win10 /f >/dev/null
+/usr/bin/wine reg add "HKCU\\Software\\Wine\\Direct3D" /v renderer /t REG_SZ /d gdi /f >/dev/null
 
 exec 6>&-
 kill $XVFB_PID
